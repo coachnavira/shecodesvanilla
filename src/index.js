c9f1c40ea205ad7681d35a386f63f06aedd6f53f
coachnavira <coachnavira@gmail.com>
2024-12-07 22:30:58 +0700

    Updated javascript

--- src/index.js
+++ src/index.js
@@ -3,8 +3,47 @@ function updateCityTemp(response) {
   let temperatureElement = document.querySelector("#current-temp");
   let temperature = response.data.temperature.current;
   let cityInput = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let description = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  let wind = response.data.wind.speed;
+  let dateElement = document.querySelector("#date");
+  let date = new Date(response.data.time * 1000);
+  let emojiElement = document.querySelector("#current-emoji");
+  let emoji = response.data.condition.icon;
+
+  console.log(response.data);
+
   cityInput.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = description;
+  humidityElement.innerHTML = humidity;
+  windElement.innerHTML = wind;
+  dateElement.innerHTML = formatDate(date);
+  emojiElement.innerHTML = emoji;
+}
+
+// format the date
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}, `;
 }
 
 // access the weather API for the particular city
